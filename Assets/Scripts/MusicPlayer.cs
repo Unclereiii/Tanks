a5e9b178322263758b2d195a5a11e393b30a4207
Unclereiii <unclereiii@gmail.com>
2020-01-17 09:04:16 +0200

    Залил основные ресурсы проекта

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+	private static MusicPlayer _instance;
+
+	public void OnEnable()
+	{
+		_instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void Awake()
+	{
+		if (_instance) DestroyImmediate(gameObject);
+	}
+}
